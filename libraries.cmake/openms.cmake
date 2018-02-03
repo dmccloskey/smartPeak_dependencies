de0b2bd5129c011984453cc21b7a88d9b31b1db7
dmccloskey <dmccloskey87@gmail.com>
2018-02-03 12:25:27 -0800

    fix: update to openms.cmake

--- libraries.cmake/openms.cmake
+++ libraries.cmake/openms.cmake
@@ -8,13 +8,15 @@ set(OPENMS_BINARY_DIR ${DEPENDENCIES_BIN_BINARY_DIR}/OpenMS)
 
 if (WIN32)
   set(OPENMS_MAKE gmake)
+  set(OPENMS_INSTALL gmake install)
 else (WIN32)
   set(OPENMS_MAKE make)
+  set(OPENMS_INSTALL make install)
 endif (WIN32)
 
 ExternalProject_Add(openms
-    GIT_REPOSITORY https://github.com/OpenMS/OpenMS
-    GIT_TAG develop
+    GIT_REPOSITORY "https://github.com/OpenMS/OpenMS"
+    GIT_TAG "develop"
     SOURCE_DIR ${OPENMS_SOURCE_DIR}
     CMAKE_ARGS 
         -DPYOPENMS=OFF
@@ -26,5 +28,5 @@ ExternalProject_Add(openms
     BUILD_IN_SOURCE ON
     BUILD_COMMAND ${OPENMS_MAKE}
     INSTALL_DIR ${INCLUDE_DIR_OPENMS}
-    INSTALL_COMMAND install
+    INSTALL_COMMAND ${OPENMS_INSTALL}
 )
